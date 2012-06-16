49122c6fa4af8a9cadc96e874ae52587c29d1409
Marco Scholl <develop@marco-scholl.de>
2012-06-16 15:12:51 +0200

    refactor

--- base/templates/includes/backend-start.cs
+++ base/templates/includes/backend-start.cs
@@ -1,1 +1,1 @@
-<?cs if:(backend.default == job_name) ?>start on resume or (local-filesystems and started udev and stopped networking)<?cs /if ?>
+<?cs if:(module.backend == job_name) ?>start on resume or (local-filesystems and started udev and stopped networking)<?cs /if ?>
